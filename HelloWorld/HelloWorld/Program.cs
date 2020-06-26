3dc57581e8a288afa78e41fa68e5462530604bda
Kevin Han <kevhan@kevins-mbp-3.attlocal.net>
2020-06-26 12:02:37 -0500

    uploading first HelloWorld program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld // namespace - 
+{
+    class Program // class - container for all different kinds of methods
+    {
+        static void Main(string[] args) // method
+        {
+            Console.WriteLine("Hello World!");
+            /*
+            Program2 hope = new Program2();
+            hope.Pickle();
+            */
+            // Console.ReadLine();
+            // comment
+        }
+    }
+
+    class Program2
+    {
+        public void Pickle(int x)
+        {
+            Console.WriteLine("It worked!");
+        }
+    }
+}

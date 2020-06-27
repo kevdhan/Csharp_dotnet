7fb7c4a6c07a777afcb5a79f603ebde72b4b23b6
Kevin Han <kevhan@kevins-mbp-3.attlocal.net>
2020-06-27 14:10:24 -0500

    new file methods

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld(); // calling method
+        }
+
+        /* Creating a new method within class Program
+         * Private - only accessible within this class? namespace?
+         * 
+        */
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}

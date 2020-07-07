7999e98c3a9be24d8c1045e072f361359f7ff18b
Kevin Han <kevhan@kevins-mbp-3.attlocal.net>
2020-07-07 16:15:10 -0500

    date and time

--- FundamentalsofCSharp/DatesAndTimes/DatesAndTimes/Program.cs
+++ FundamentalsofCSharp/DatesAndTimes/DatesAndTimes/Program.cs
@@ -12,7 +12,35 @@ namespace DatesAndTimes
 
             // Console.WriteLine(myValue.ToShortDateString()); // only show date, not time
 
-            Console.WriteLine(myValue.ToShortTimeString());
+            // Console.WriteLine(myValue.ToShortTimeString());
+
+            // Console.WriteLine(myValue.ToLongTimeString());
+
+            // adding days to date
+            // Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+
+            // adding hours to time
+            // Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+
+            // to subtract hours, add negatie hours
+            // Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            // Console.WriteLine(myValue.Month);
+
+
+            // creating custom datetime --> remember to convert to string before printing
+            /*
+            DateTime myBirthday = new DateTime(1996, 12, 31);
+            Console.WriteLine(myBirthday.ToShortDateString());
+            */
+
+
+            // parsing string to DateTime
+            DateTime myBirthday = DateTime.Parse("12/31/1996"); // parsing string to DateTime
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday); // creating a new class called TimeSpan
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
 
         }
     }

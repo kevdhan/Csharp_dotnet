24597356cf310df5bed48a541a894dae350fd973
Kevin Han <kevhan@kevins-mbp-3.attlocal.net>
2020-07-07 15:28:47 -0500

    restructuring of folders

---
+++ FundamentalsofCSharp/DatesAndTimes/DatesAndTimes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now; // get the date and time of now
+
+            // Console.WriteLine(myValue.ToString());
+
+            // Console.WriteLine(myValue.ToShortDateString()); // only show date, not time
+
+            Console.WriteLine(myValue.ToShortTimeString());
+
+        }
+    }
+}

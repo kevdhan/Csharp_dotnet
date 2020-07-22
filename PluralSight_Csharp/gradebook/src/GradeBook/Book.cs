6788ae1bd9b2df52b6adbbfda5b70c9122bbe4d9
Kevin Han <kevhan@kevins-mbp-3.home>
2020-07-22 10:39:48 -0500

    GradeBook - created a grade book application. Created a separate book class

---
+++ PluralSight_Csharp/gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name) // constructor method
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        /* Method to add grades to grades List
+         */
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        /* Method to print out lowest, highest, average grades
+         */
+        public void ShowStatistics()
+        {
+            var avgGrade = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+                avgGrade += grade;
+            }
+            avgGrade /= grades.Count;
+
+            Console.WriteLine($"Lowest Grade: {lowGrade}");
+            Console.WriteLine($"Highest Grade: {highGrade}");
+            Console.WriteLine($"Average Grade: {avgGrade}");
+        }
+    }
+}
--- PluralSight_Csharp/gradebook/src/GradeBook/Program.cs
+++ PluralSight_Csharp/gradebook/src/GradeBook/Program.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         // input of Main is an array of strings
         static void Main(string[] args) 
         {
-            if (args.Length > 0)
-            {
-                Console.WriteLine("Hello, {0}!", args[0]);
-            }
-            else
-            {
-                Console.WriteLine("Hello");
-            }
-        }
+
+            // var data type has to equal something
+            // var - still strongly defined. Can't define a var variable as a double and a string
+
+            // var numbers = new double[3]; | creating a double array
+            // var numbers = new double[] {12.7, 11.3, 15.4};
+            // var numbers = new[] {12.7, 11.3, 15.8}; | here, creating an array of doubles, without officially declaring double
+
+
+            // Array - specific size
+            // Collections - fluid size
+
+
+
+            // utilizing the new class Book
+            var book = new Book("Kevin's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics(); // want this method to show lowest, highest, average grades
+
+            /* By using abstraction and encapsulation we were able to make the code much cleaner
+             * Now, if we were to come back to this code 5 months or years later, we can easily see what we are doing here
+             */
+
+
+    }
     }
 }

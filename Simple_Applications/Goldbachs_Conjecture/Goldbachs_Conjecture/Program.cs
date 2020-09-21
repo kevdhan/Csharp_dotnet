615cabdba898cb2e2d0a4e6b46abf053f7dc0cbb
Kevin Han <kevhan@dhcp-10-154-146-233.vpn.oracle.com>
2020-09-21 12:09:58 -0500

    finished implementing Goldbach's Algorithm

--- Simple_Applications/Goldbachs_Conjecture/Goldbachs_Conjecture/Program.cs
+++ Simple_Applications/Goldbachs_Conjecture/Goldbachs_Conjecture/Program.cs
@@ -6,33 +6,64 @@ namespace Goldbachs_Conjecture
     {
         static void Main(string[] args)
         {
+            Console.Clear();
 
-            Console.WriteLine("Testing Goldbach's Conjecture: Every even number greater than or" +
+            // initial query for input from user
+            Console.WriteLine("Testing Goldbach's Conjecture: Every even number greater than or " +
                 "equal to 4 can be expressed as the sum of two prime numbers.");
-            Console.WriteLine("Enter the lower limit (Even Number, >= 4): ");
+            Console.Write("Enter the lower limit (Even Number, >= 4): ");
             var lower_input = Console.ReadLine();
             var lower = int.Parse(lower_input);
-            Console.WriteLine("Enter the upper limit (Even Number): ");
+            Console.Write("Enter the upper limit (Even Number): ");
             var upper_input = Console.ReadLine();
             var upper = int.Parse(upper_input);
 
+            // loop, until write response is given
+            // even number, greater or equal than 4, where lower number must be smaller or equal
+            // to upper 
             while ((lower<4) || (lower%2 != 0) || (upper%2!=0) || (lower>=upper))
             {
-                Console.WriteLine("Enter the lower limit (Even Number, >= 4): ");
+                Console.Write("Enter the lower limit (Even Number, >= 4): ");
                 lower_input = Console.ReadLine();
                 lower = int.Parse(lower_input);
-                Console.WriteLine("Enter the upper limit (Even Number): ");
+                Console.Write("Enter the upper limit (Even Number): ");
                 upper_input = Console.ReadLine();
                 upper = int.Parse(upper_input);
             }
+
+            Goldbachs_Alg(lower, upper);
 
         }
 
         public static void Goldbachs_Alg(int lower, int upper)
         {
-            
+            for (int userrange = lower; userrange < upper+1; userrange+=2)
+            {
+                Console.Write(userrange + " ");
+                for (int lowprime = 2; lowprime < (userrange/2)+1; lowprime++)
+                {
+                    if (is_prime(lowprime) && (lowprime*2 == userrange))
+                    {
+                        Console.Write("=" + lowprime + "+" + lowprime + " ");
+                    }
+                    for (int highprime = (userrange/2)+1; highprime < userrange; highprime++)
+                    {
+                        if (is_prime(lowprime) && is_prime(highprime)
+                            && (lowprime+highprime) == userrange)
+                        {
+                            Console.Write("=" + lowprime + "+" + highprime + " ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
         }
 
+        /// <summary>
+        /// takes in a number and determines if it is prime
+        /// </summary>
+        /// <param name="numb"></param>
+        /// <returns>bool, if number is prime, return true, otherwise false</returns>
         public static bool is_prime(int numb)
         {
             if (numb == 1)

2bd765e2dfe67b927db0c23db4bc38d29846b8a8
Kevin Han <kevhan@Kevins-MacBook-Pro-3.local>
2020-09-21 13:51:37 -0500

    Created a simpe application called Deal that tests Marilyn von Savant's theory for the Lets Make a Deal game

---
+++ Simple_Applications/Deal/Deal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Deal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask user how many times they want to play
+            Console.Write("Enter number of times you want to play: ");
+            var numgames = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{"Prize", -10} {"Guess", -10} {"View", -10} {"New Guess", -10}");
+
+            var numwins = 0;
+
+            Random randnum = new Random();
+
+            // User guess, prize, and view is randomly generated
+            for (int i = 1; i < numgames+1; i++)
+            {
+                var prize = randnum.Next(1, 4);
+                var guess = randnum.Next(1, 4);
+                var view = randnum.Next(1, 4);
+                while (view == prize || view == guess)
+                {
+                    view = randnum.Next(1, 4);
+                }
+
+                var newGuess = randnum.Next(1, 4);
+                while (newGuess == guess || newGuess == view)
+                {
+                    newGuess = randnum.Next(1, 4);
+                }
+
+                Console.WriteLine($"{prize, 3} {guess, 10} {view, 9} {newGuess, 12}");
+
+                if (newGuess == prize)
+                {
+                    numwins++;
+                }
+            }
+
+            Console.WriteLine($"Probability of winning if you switch = "
+                + string.Format("{0:0.##}", numwins/numgames));
+
+            Console.WriteLine($"Probability of winning if you DO NOT switch = "
+                + string.Format("{0:0.##}", 1 - (numwins / numgames)));
+        }
+    }
+}
---
+++ Simple_Applications/Deal/README.md
@@ -0,0 +1,11 @@
+deal
+
+Stimulating the game of "Let's Make a Deal" game and test Marilyn Vos Savant's theory.
+Her theory is as follows:
+There are 3 doors, with a prize behind one. When you choose a door, another door that is not the one you selected and does not have the prize will be opened (essentially taking it out of the choices to choose from). Before the door with the prize is opened, you have the option to switch doors. Is it better for you to switch doors? Marilyn's theory was yes, it is always better to switch doors as it increases your chance of winning.
+
+Steps:
+1. Input the number of times you want the game simulation to run
+2. The door # with the prize, the simulated guess, the door without the prize, and the new guess (switching doors) will be printed for each round.
+3. Lastly, the probability of winning if you switch and do not switch will be printed out
+

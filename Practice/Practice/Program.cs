39181f92cb8c851a5eb243703b4ef168f267b20d
Kevin Han <kevhan@Kevins-MacBook-Pro-3.local>
2020-10-30 13:48:46 -0500

    Created Practice Console App to use as a placeholder for practicing general c#/.NET concepts

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            decimal decValue = 50.0m;
+
+            //
+        }
+    }
+}
